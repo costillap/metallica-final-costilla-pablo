61b70a1786f9f0168eecaf53494a9439bab9903b
costillap <costillap@hotmail.com.ar>
2022-07-21 22:40:03 -0300

    primera-entrega

--- src/components/item.jsx
+++ src/components/item.jsx
@@ -1,15 +1,15 @@
 import { Link } from "react-router-dom";
 
 const Item = ({item}) => {
-    const {title, stock, category, detalle, id } = item;
+    const {title, stock, category, detalle, id, img } = item;
     return(
     <div className="card" >
-        <img src="..." className="card-img-top" alt="..."/>
+        <img src={img} className="card-img-top" alt="..."/>
         <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{stock}</p>
             <p className="card-text">{category}</p>
-            <Link to={`item/${id}`} className="btn btn-primary"></Link>
+            <Link to={`item/${id}`} className="btn btn-primary">Mas</Link>
         </div>
     </div>
     )
--- src/components/itemDetail.jsx
+++ src/components/itemDetail.jsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 
 const ItemDetail = ({item} ) => {
-    const {title, stock, category, detalle} = item;
+    const {title, stock, category, detalle, img} = item;
     return(
     <div className="container">
         <div className="card col-6 mx-auto" >
-            <img src="..." className="card-img-top" alt="..."/>
+            <img src={img} className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
                 <p className="card-text">{stock}</p>
