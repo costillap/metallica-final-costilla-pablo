e84b5d79ab7dc409864ac5f580b4a0f9006428b5
costillap <costillap@hotmail.com.ar>
2022-08-11 23:01:14 -0300

    itemcollection

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,29 +1,58 @@
 import React from "react";
 import { useState, useEffect} from "react";
 import ItemList from "./ItemList";
-import data from "./utils/Data.json"
-
+import {getFirestore, collection, getDocs } from "firebase/firestore"
 
 const ItemListContainer = ({greeting}) => {
     const [items, setItems] = useState([]);
-    const promise = new Promise((resolve) => {
-        setTimeout( () => resolve(data), 1000);
-    });
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
-        promise.then((res) => setItems(data));
-        }, []);
+        setLoading(true)
+        const db = getFirestore();
+        const itemColletions = collection(db, "items");
+        const itemsDocuments = getDocs(itemColletions).then((snapshot) => {
+            const data = snapshot.docs.map((doc) => ({id: doc.id, ...doc.data()}));
+            setItems(data);
+            setLoading(false)
+        });
+    }, []);
     return ( 
     <>
         <p className="container text-light fs-2 text-center p-5 m-5 mx-auto ">{greeting}</p>
         <div className="mt-5">
             <ItemList items={items}/>
         </div>
-        
     </>
-        
-     );
+    );
 }
- 
 export default ItemListContainer;
 
+
+
+
 /**/
+ /*const db = getFirestore();
+        const docRef = doc(db, "tiems", "Ac1INFbo2PFozwdfLu0c");
+        getDoc(docRef).then((snapshot) => {
+            const data = { id: snapshot.id, ...snapshot.data()};
+       });*/
+
+
+
+      
+
+        /*const db = getFirestore();
+        const itemColletions = collection(db, "items");
+        const filteredCollection = query(
+            itemColletions, 
+            where("title", "==", "Master of Puppets")
+        );
+        getDocs(filteredCollection).then(snapshot => {
+            const data = snapshot.docs.map(doc => ({
+                id: doc.id,
+                ...doc.data(),
+            }))
+            console.log(data);
+        });
+        */
+        
--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAs7DGzoBViGbY1MGzkJuKo11f4w_xMlUo",
+  authDomain: "metallica-aafa2.firebaseapp.com",
+  projectId: "metallica-aafa2",
+  storageBucket: "metallica-aafa2.appspot.com",
+  messagingSenderId: "8251596808",
+  appId: "1:8251596808:web:41fd425b3c3a513f29b1bd"
+};
 
+initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
